216c82f7710fbb1dc7a391b9400a94acd3ae7dd4
milhot <93010599+milhot@users.noreply.github.com>
2023-05-19 14:44:23 +0300

    Update main.c
    delete string

--- main.c
+++ main.c
@@ -2,13 +2,9 @@
 
 int main(int argc, char **argv)
 {
-	char first[255], last[255];
-
 	printf("Enter your first name: ");
 	fgets(first, 255, stdin);
 	first[strlen(first) - 1] = '\0'; /* remove the newline at the end */
-
-	printf("Hello %s %s!\n", first, last);
 
 	printf("Hello world!\n");
 	return 0;
